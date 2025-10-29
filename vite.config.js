403ea1f9bfa382a8f4ee509cd21160edc0aba094
Lemuel-Jebas-K-URK23CS1172 <klemuel@karunya.edu.in>
2025-10-29 23:11:58 +0530

    Add GitHub Actions deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/Farm_To_Table_Game/',
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:4000",
-    },
-  },
+  base: "/Farm_To_Table_Game/", // 👈 must match your repo name
 });
