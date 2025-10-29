57100f653c4c7514c3049ec0d48352e08143a606
Lemuel-Jebas-K-URK23CS1172 <klemuel@karunya.edu.in>
2025-10-30 00:13:10 +0530

    Fix router and base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Farm_To_Table_Game/", // 👈 must match your repo name
-});
+  base: '/Farm_To_Table_Game/', // 👈 MUST MATCH your repo name exactly (case sensitive)
+})
