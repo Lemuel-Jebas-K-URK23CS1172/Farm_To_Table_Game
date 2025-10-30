01ea6d673fdbefb150bb46abf7372b4350c4176d
K Lemuel Jebas URK23CS1172 <153975153+Lemuel-Jebas-K-URK23CS1172@users.noreply.github.com>
2025-10-30 23:44:48 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// client/vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/FarmToTableGame/", // 👈 must match your repo name exactly
+  base: "/FarmToTableGame/", // ⚠️ must match your repo name exactly (case-sensitive)
 });
