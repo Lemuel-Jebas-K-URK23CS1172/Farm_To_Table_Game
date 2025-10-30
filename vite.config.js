a8e91991df4277154b299ef93f8bf7a69c303bf6
Lemuel-Jebas-K-URK23CS1172 <klemuel@karunya.edu.in>
2025-10-30 08:18:55 +0530

    initial

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Farm_To_Table_Game/", // 👈 must match your repo name exactly
+  base: "/FarmToTableGame/", // 👈 must match your repo name exactly
 });
