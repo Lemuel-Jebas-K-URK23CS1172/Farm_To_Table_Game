82489b02f7efcc470f18b2b6eced997f8a904d74
K Lemuel Jebas URK23CS1172 <153975153+Lemuel-Jebas-K-URK23CS1172@users.noreply.github.com>
2025-11-01 21:53:51 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
-import App from "./App";
-import { AuthProvider } from "./contexts/AuthContext";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HashRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <App />
     </HashRouter>
   </React.StrictMode>
 );
