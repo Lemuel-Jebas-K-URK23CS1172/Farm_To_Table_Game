6bff13b65a64e14eda064544b70c435bab41ad4b
K Lemuel Jebas URK23CS1172 <153975153+Lemuel-Jebas-K-URK23CS1172@users.noreply.github.com>
2025-11-02 20:41:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,34 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 
-// Import pages
+// Pages
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import GamePage from "./pages/GamePage";
 import Dashboard from "./pages/Dashboard";
 import AdminDashboard from "./pages/AdminDashboard";
+import NotFound from "./pages/NotFound";
 
 export default function App() {
   return (
-    <React.StrictMode>
-      <AuthProvider>
-        <BrowserRouter>
-          <Routes>
-            {/* Public routes */}
-            <Route path="/" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+    <AuthProvider>
+      <BrowserRouter basename="/FarmToTableGame">
+        <Routes>
+          {/* Public routes */}
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
 
-            {/* Protected routes */}
-            <Route path="/game" element={<GamePage />} />
-            <Route path="/dashboard" element={<Dashboard />} />
+          {/* Protected user routes */}
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/game" element={<GamePage />} />
 
-            {/* Admin route */}
-            <Route path="/admin" element={<AdminDashboard />} />
-          </Routes>
-        </BrowserRouter>
-      </AuthProvider>
-    </React.StrictMode>
+          {/* Admin route */}
+          <Route path="/admin" element={<AdminDashboard />} />
+
+          {/* Catch-all route */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
