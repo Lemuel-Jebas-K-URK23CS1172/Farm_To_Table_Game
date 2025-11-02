0075d9a97e3f0c8d8f410b25e49818a299d29710
K Lemuel Jebas URK23CS1172 <153975153+Lemuel-Jebas-K-URK23CS1172@users.noreply.github.com>
2025-11-02 21:31:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,25 @@
 // src/App.jsx
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 
-// Pages
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import GamePage from "./pages/GamePage";
 import Dashboard from "./pages/Dashboard";
 import AdminDashboard from "./pages/AdminDashboard";
-import NotFound from "./pages/NotFound";
 
 export default function App() {
   return (
     <AuthProvider>
-      <BrowserRouter basename="/FarmToTableGame">
+      <Router>
         <Routes>
-          {/* Public Routes */}
           <Route path="/" element={<Login />} />
           <Route path="/register" element={<Register />} />
-
-          {/* User Routes */}
-          <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/game" element={<GamePage />} />
-
-          {/* Admin Route */}
+          <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/admin" element={<AdminDashboard />} />
-
-          {/* 404 */}
-          <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </AuthProvider>
   );
 }
