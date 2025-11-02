717d11eddff8b1575b4d164eed07c899d68959dc
K Lemuel Jebas URK23CS1172 <153975153+Lemuel-Jebas-K-URK23CS1172@users.noreply.github.com>
2025-11-02 20:53:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Replace with your repo name (case-sensitive)
 export default defineConfig({
   plugins: [react()],
-  base: "/FarmToTableGame/",
-  build: {
-    outDir: "dist",
-    sourcemap: false,
-  },
-  server: {
-    port: 5173,
-    open: true,
-  },
+  base: "/FarmToTableGame/", // 👈 very important for GitHub Pages
 });
