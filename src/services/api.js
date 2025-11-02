553685330193781a30f95b1588d25ae037e826b8
K Lemuel Jebas URK23CS1172 <153975153+Lemuel-Jebas-K-URK23CS1172@users.noreply.github.com>
2025-11-02 23:43:20 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ export const API = axios.create({
   baseURL: "https://farmtotablegameserver-production.up.railway.app/api",
 });
 
-// ✅ Automatically attach token
+// attach JWT
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
